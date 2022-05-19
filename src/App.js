96c0eef6c61e8b81e3c9aacb6ac627e9d4e10693
savawedev <andrei.sava.dev@gmail.com>
2022-05-19 21:14:10 +0300

    Changed API calls to App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import getPopularMovies from "./data/getPopularMovies";
+import getPopularShows from "./data/getPopularShows";
 import Footer from "./components/Footer/Footer";
 import GlobalStyle from "./GlobalStyle";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [popularShows, setPopularShows] = useState([]);
+
+  useEffect(() => {
+    getPopularMovies().then((data) => {
+      setPopularMovies(data.results);
+    });
+
+    getPopularShows().then((data) => {
+      setPopularShows(data.results);
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              movies={popularMovies.slice(0, 20)}
+              shows={popularShows.slice(0, 20)}
+            />
+          }
+        />
       </Routes>
 
       <Footer />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,13 @@
-import { useState, useEffect } from "react";
-import getPopularMovies from "../data/getPopularMovies";
-import getPopularShows from "../data/getPopularShows";
 import Hero from "../components/Hero/Hero";
 import PopularMovies from "../components/HomePage/PopularMovies";
 import PopularShows from "../components/HomePage/PopularShows";
 
-const Home = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [popularShows, setPopularShows] = useState([]);
-
-  useEffect(() => {
-    getPopularMovies().then((data) => {
-      setPopularMovies(data.results.slice(0, 20));
-    });
-
-    getPopularShows().then((data) => {
-      setPopularShows(data.results.slice(0, 20));
-    });
-  }, []);
-
+const Home = ({ movies, shows }) => {
   return (
     <>
       <Hero />
-      <PopularMovies data={popularMovies} />
-      <PopularShows data={popularShows} />
+      <PopularMovies data={movies} />
+      <PopularShows data={shows} />
     </>
   );
 };
